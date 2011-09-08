bf82a94aa6917d83283cfc89ce542df5f79e55be
redtower <redtower@gmail.com>
2011-09-08 09:19:35 +0900

    using の整理（System：Console, System.IO：File, System.Diagostics：Process）

--- open.cs
+++ open.cs
@@ -1,12 +1,13 @@
 using System;
 using System.IO;
+using System.Diagnostics;
 
 class Open {
     public static void Main(string[] args) {
         if (args.Length != 0) {
             foreach (string arg in args) {
                 if (File.Exists(arg)) {
-                    System.Diagnostics.Process.Start(arg);
+                    Process.Start(arg);
                 } else {
                     Console.WriteLine("[ERROR] File Not Found : " + arg);
                 }
