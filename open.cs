dc17f075f9bc933c1c8144e4ae5d5a7b31a01015
redtower <redtower@gmail.com>
2011-09-15 18:32:52 +0900

    関連付けがないファイルをオープンしたときのエラー処理を追加

--- open.cs
+++ open.cs
@@ -7,7 +7,11 @@ class Open {
         if (args.Length != 0) {
             foreach (string arg in args) {
                 if (File.Exists(arg)) {
-                    Process.Start(arg);
+                    try {
+                        Process.Start(arg);
+                    } catch (System.ComponentModel.Win32Exception e) {
+                        Console.WriteLine("[ERROR] " + arg + " : " +  e.Message);
+                    }
                 } else {
                     Console.WriteLine("[ERROR] File Not Found : " + arg);
                 }
