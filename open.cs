4852817becc971f5ed3c978322e5cdb3486a29c8
redtower <redtower@gmail.com>
2012-01-11 09:32:35 +0900

    ディレクトリオープン時のエラー処理を追加

--- open.cs
+++ open.cs
@@ -13,7 +13,11 @@ class Open {
                         Console.WriteLine("[ERROR] " + arg + " : " +  e.Message);
                     }
                 } else if (Directory.Exists(arg)) {
+                    try {
                         Process.Start("EXPLORER.EXE", "/n, " + arg);
+                    } catch (System.ComponentModel.Win32Exception e) {
+                        Console.WriteLine("[ERROR] " + arg + " : " +  e.Message);
+                    }
                 } else {
                     Console.WriteLine("[ERROR] File Not Found : " + arg);
                 }
