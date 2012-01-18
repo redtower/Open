5aa40797e8992d9d2ec879ee53d2e60909677244
redtower <redtower@gmail.com>
2012-01-18 11:35:00 +0900

    System.ComponentModelをusing ディレクティブで定義する

--- open.cs
+++ open.cs
@@ -1,21 +1,22 @@
-using System;
-using System.IO;
-using System.Diagnostics;
+using System;               // Console
+using System.IO;            // File,Directory
+using System.Diagnostics;   // Process
+using System.ComponentModel;// Win32Exception
 
 class Open {
     public static void Main(string[] args) {
         if (args.Length != 0) {
             foreach (string arg in args) {
-                if (File.Exists(arg)) {
+                if (File.Exists(arg)) { // ファイルの時
                     try {
                         Process.Start(arg);
-                    } catch (System.ComponentModel.Win32Exception e) {
+                    } catch (Win32Exception e) {
                         Console.WriteLine("[ERROR] " + arg + " : " +  e.Message);
                     }
-                } else if (Directory.Exists(arg)) {
+                } else if (Directory.Exists(arg)) { // ディレクトリの時
                     try {
                         Process.Start("EXPLORER.EXE", "/n, " + arg);
-                    } catch (System.ComponentModel.Win32Exception e) {
+                    } catch (Win32Exception e) {
                         Console.WriteLine("[ERROR] " + arg + " : " +  e.Message);
                     }
                 } else {
